bb4e377e0083c9516603f97eaea1d4d4a57e8093
hominhhau <hauxoan333@gmail.comhau>
2023-10-25 01:03:27 +0700

    add Frame ProcessLoading, add mssql jdbc , connectDB, check connection in class Application.....

--- src/main/java/gui/Application.java
+++ src/main/java/gui/Application.java
@@ -1,9 +1,65 @@
 package gui;
 
-import gui.form.Form_Home;
+import connectDB.ConnectDB;
+import gui.form.Form_Login;
+import gui.form.ProcessesLoading;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import javax.swing.*;
 
+/**
+ *
+ * @author HO MINH HAU
+ */
 public class Application {
 
-    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                ConnectDB db = ConnectDB.getInstance();
+                ProcessesLoading processesLoading = new ProcessesLoading();
+                Form_Login formLogin = new Form_Login();
+                // Hiển thị giao diện "ProcessesLoading"
+                processesLoading.setVisible(true);
+
+                // Kết nối đến cơ sở dữ liệu
+                try {
+                    db.connect();
+                    if (db.isConnected()) {
+                        System.out.println("Connection Success with database KaraokeAPLUS");
+                        // Đợi 3 giây trước khi ẩn "ProcessesLoading" và hiển thị "Form_Login"
+                        Timer timer = new Timer(3000, new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                // Ẩn giao diện "ProcessesLoading"
+                                processesLoading.setVisible(false);
+                                // Hiển thị giao diện "Form_Login"
+                                formLogin.setVisible(true);
+                            }
+                        });
+                        timer.setRepeats(false); // Đảm bảo chạy một lần duy nhất
+                        timer.start();
+                    } else {
+                        System.out.println("Connection failed!");
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+
+                    Timer timer = new Timer(50000, new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            // Ẩn giao diện "ProcessesLoading" chờ hơn 5 phút thì thoát app
+                            processesLoading.setVisible(false);
+                        }
+                    });
+                    timer.setRepeats(false);
+                    timer.start();
+                    System.out.println("Connection failed! <<check databasename, user, password>>" + e.getMessage());
+                }
+            }
+        });
+    }
+
 }
---
+++ src/main/java/gui/event/EventCheckConnectDB.java
@@ -0,0 +1,10 @@
+
+package gui.event;
+
+/**
+ *
+ * @author HO MINH HAU
+ */
+public interface EventCheckConnectDB {
+    public  void EvenCheckConnectDB();
+}
